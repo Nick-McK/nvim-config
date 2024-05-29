3db710e016ae4773151c352ce39cfb854505201d
Nick-McK <nickmckeemanmodels@gmail.com>
2024-05-29 23:02:47 +0100

    Added list chars and default to spaces over tabs

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -18,9 +18,8 @@ opt.numberwidth = 2
 opt.ruler = false
 opt.relativenumber = true
 
-
 -- indenting
-opt.expandtab = false
+opt.expandtab = true
 opt.shiftwidth = 4 -- this is the actual indenting size
 opt.smartindent = true
 opt.autoindent = true
@@ -40,7 +39,8 @@ opt.scrolloff = 8
 -- disable vim startup - this might not be needed, as long as we don't print anything
 -- just leave printing and info to the dashboard on startup
 -- opt.shortmess = "ltToOCF"
-
+-- ← • ♡
+opt.listchars ={tab="❮—❯", space="•"}
 
 -- Word wrapping with h and l
 -- opt.whichwrap:append "<>[]hl"
