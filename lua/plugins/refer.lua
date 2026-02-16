11652cb9c641c9c82b81952c970ef78afeaf86c2
Nick <nick@nickm.home>
2026-02-16 23:45:08 +0000

    feat(refer): added frecency searching + toggling hidden file searches

--- lua/plugins/refer.lua
+++ lua/plugins/refer.lua
@@ -1,3 +1,27 @@
+local function update_frecency_blink(selection)
+  if type(selection) ~= "string" or selection == "" then
+    vim.notify("NO SELECTION", vim.log.levels.ERROR)
+    return
+  end
+
+  local ok, fuzzy = pcall(require, "blink.cmp.fuzzy")
+  if not ok then
+    vim.notify("NO BLINK FUZ", vim.log.levels.ERROR)
+    return
+  end
+
+  -- This function updates the frecency database
+  fuzzy.access({
+    label = selection,
+    filterText = selection,
+    sortText = selection,
+    insertText = selection,
+    kind = 1,
+    score_offset = 0,
+    source_id = "refer",
+  })
+end
+
 return {
   "juniorsundar/refer.nvim",
   dependencies = {
@@ -5,7 +29,8 @@ return {
     -- "nvim-mini/mini.fuzzy",
   },
   config = function()
-    require("refer").setup({
+    local refer = require("refer")
+    refer.setup({
       -- General Settings
       max_height_percent = 0.4,-- Window height as a percentage of editor height
       min_height = 1,         -- Minimum window height in lines
@@ -14,9 +39,41 @@ return {
       debounce_ms = 10,      -- Delay for async searching (files/grep)
       min_query_len = 2,      -- Minimum characters to start async search
 
+      custom_sorters = {
+        blink_frecency = function(items, query)
+          local blink_ok, blink = pcall(require, "refer.blink")
+          if not blink_ok then return items end
+          if not blink.is_available() then return items end
+
+          local blink_items = {}
+          for _, item in ipairs(items) do
+            blink_items[#blink_items + 1] = {
+              label = item,
+              filterText = item,
+              sortText = item,
+              insertText = item,
+              kind = 1,
+              score_offset = 0,
+              source_id = "refer",
+            }
+          end
+
+          blink.set_provider_items("refer", blink_items)
+          local _, idxs = blink.fuzzy(query, "refer")
+          if not idxs then return {} end
+
+          local out = {}
+          for _, idx in ipairs(idxs) do
+            out[#out + 1] = items[idx + 1]
+          end
+          return out
+        end
+      },
+
       -- Sorting
-      available_sorters = { "blink", "mini", "native", "lua" },
-      default_sorter = "blink", -- Default sorter for static lists.
+      -- available_sorters = { "blink_frecency", "blink", "mini", "native", "lua" },
+      available_sorters = {"blink_frecency", "blink"},
+      default_sorter = "blink_frecency", -- Default sorter for static lists.
       -- If blink.cmp isn't installed, it will download
       -- the compiled library using `curl`.
 
@@ -44,7 +101,14 @@ return {
           -- Custom ignored directories for 'Files' picker
           ignored_dirs = { ".git", ".jj", "node_modules", ".cache" },
           -- Custom fd command
-          find_command = { "fdfind", "-H", "--type", "f", "--color", "never" },
+          -- find_command = { "fdfind", "-H", "--type", "f", "--color", "never" },
+          find_command = function(query)
+            local cmd = { "fdfind", "-H", "--type", "f", "--color", "never"}
+            if vim.g.refer_show_ignored then
+              table.insert(cmd, "-u") -- stop ignore
+            end
+            return cmd
+          end
         },
         grep = {
           -- Custom rg command
@@ -60,6 +124,11 @@ return {
         ["<Down>"] = "next_item",
         ["<Up>"] = "prev_item",
         ["<CR>"] = "select_input",
+        -- Select input and update the frecency
+        ["<C-space>"] = function (selection, builtin)
+          update_frecency_blink(selection) -- otherwise the frecency won't work
+          builtin.actions.select_entry()
+        end,
         ["<Esc>"] = "close",
         ["<C-c>"] = "close",
         ["<C-g>"] = "send_to_grep",
@@ -71,6 +140,13 @@ return {
         ["<C-a>"] = "select_all",
         ["<C-x>"] = "deselect_all",
         ["<M-t>"] = "toggle_all",
+        -- Toggle hidden files
+        ["<M-i>"] = function(selection, builtin)
+          vim.g.refer_show_ignored = not vim.g.refer_show_ignored
+          if builtin and builtin.actions and builtin.actions.refresh then
+            builtin.actions.refresh()
+          end
+        end
 
       },
     })
