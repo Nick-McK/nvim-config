4595f949774ccb1f7948a68b3923da6d0993c380
Nick McKeeman <nickmckeemanmodels@gmail.com>
2026-07-29 10:48:42 +0100

    bufbuddy keybinds + colorscheme update

--- colors/my_theme.lua
+++ colors/my_theme.lua
@@ -1,29 +1,63 @@
-local function hl(name, data)
-  vim.api.nvim_set_hl(0, name, data)
-end
+local M = {}
+local highlights = {}
+
+vim.cmd("highlight clear")
 
 local c = {
-  offwhite = "#dfdfaf",
-  yellow_muted = "#ffe6b5",
+  background="#101010",
+  white = "#D8DEE9",
+  -- white = "#f2e5bc",
+  -- offwhite = "#dfdfaf",
+  -- offwhite = "#e7e6d0",
+  offwhite = "#fffee2",
+  -- yellow_muted = "#ffe6b5",
+  yellow_muted = "#fffd96",
   violet = "#b294bb",
   purple_vivid = "#8e6fbd",
   grey = "#878787",
   red = "#c98996",
+  red_soft = "#af8787",
+  red_dark = "#875f5f",
   purple_soft = "#ab75ae",
   green = "#86ae75"
 }
 
-hl("Normal",                                        { fg=c.offwhite, bg="#101010" })
-hl("Function",                                      { fg=c.yellow_muted })
+highlights.base = {
+  ["Normal"] = { fg=c.offwhite, bg=c.background },
+  ["Function"] = { fg=c.yellow_muted },
+  ["Statement"] = { fg=c.violet },
+  ["String"] = { fg=c.green },
+  ["Type"] = { fg=c.red_dark},
+  ["@constant"] = { fg=c.red },
+  ["@lsp.type.parameter"] = {fg=c.grey},
+  ["@lsp.type.property"] = {fg=c.offwhite}
+}
 
-hl("@constant",                                     { fg=c.red })
-hl("@lsp.type.parameter",                           { fg=c.purple_soft })
+-- Language specifics
 
-hl("@string",                                       { fg=c.green })
+-- hl("@constant",                                     { fg=c.red })
+-- -- hl("@lsp.mod.readonly.typescript",                                     { fg=c.red })
+-- hl("@lsp.type.parameter",                           { fg=c.offwhite })
+--
+-- hl("@string",                                       { fg=c.green })
+--
+-- hl("@keyword",                                      { fg=c.violet })
+-- hl("@keyword.faded",                                { fg=c.violet })
+-- hl("Statement",                                      { fg=c.violet })
+--
+-- hl("@variable.member",                              { fg=c.violet })
+-- hl("@variable",                                     { fg=c.offwhite })
 
-hl("@keyword",                                      { fg=c.violet })
-
-hl("@variable.member",                              { fg=c.violet })
-hl("@variable",                                     { fg=c.offwhite })
 
 
+M.setup = function()
+  for _, highlight in pairs(highlights) do
+    for name, opts in pairs(highlight) do
+      vim.api.nvim_set_hl(0, name, opts)
+    end
+  end
+end
+
+M.setup()
+
+return M
--- lua/plugins/ui/BufBuddy.lua
+++ lua/plugins/ui/BufBuddy.lua
@@ -1,5 +1,60 @@
 return {
   dir = "~/dev/plugins/BufBuddy/",
+  -- opts = {
+  --   use_absolute_paths = true,
+  --   buffers_in_tabline = true,
+  --   float = {
+  --     height = "auto",
+  --     width = 110,
+  --     title = "Buffers"
+  --   },
+  --   split = {
+  --     width_type = "auto", -- auto or static. auto will keep each file on 1 line. static uses width
+  --     width = 10, -- only applies to static width type
+  --     direction = "left",
+  --     identifier = "―‣" -- string used to indicate which buffer is active
+  --
+  --   },
+  --   mappings = {
+  --     -- Opens a floating window showing the list of buffers
+  --     open_float = "<C-a>",
+  --     -- Opens a split containing the list of buffers
+  --     open_split = "<A-e>",
+  --     -- Opens a window showing buffers at the cursor. This type has no window opts
+  --     -- because the width and height are dependent on the height and length of the
+  --     -- contained buffers
+  --     open_cursor = ",",
+  --
+  --     -- Cycle to the next buffer in the list. Wraps around at the start
+  --     next = "",
+  --     -- Cycle to the previous buffer in the list. Wraps around at the end
+  --     prev = "",
+  --
+  --     float = {
+  --       quit = "q",
+  --       select = "<CR>"
+  --     },
+  --     split = {
+  --       quit = "q",
+  --       select = "<CR>"
+  --     },
+  --     cursor = {
+  --       quit = "q",
+  --       select = "<CR>"
+  --     }
+  --   }
+  -- },
+  -- keys = {
+  --   {
+  --     "<Tab>", function() require("BufBuddy"):open_ui("float") end, desc="BufBuddy"
+  --   },
+  --   {
+  --     "]b", function() require("BufBuddy"):next() end, desc="Next Buffer"
+  --   },
+  --   {
+  --     "[b", function() require("BufBuddy"):prev() end, desc="Prev Buffer"
+  --   }
+  -- }
   config = function()
     ---@class Manager
     local buf_buddy = require("BufBuddy").setup({
@@ -7,7 +62,7 @@ return {
       buffers_in_tabline = true,
       float = {
         height = "auto",
-        width = 110,
+        width = "auto",
         title = "Buffers"
       },
       split = {
@@ -19,7 +74,7 @@ return {
       },
       mappings = {
         -- Opens a floating window showing the list of buffers
-        open_float = "<C-a>",
+        -- open_float = "<C-a>",
         -- Opens a split containing the list of buffers
         open_split = "<A-e>",
         -- Opens a window showing buffers at the cursor. This type has no window opts
@@ -47,10 +102,11 @@ return {
       }
 
     })
+    vim.keymap.set("n", "<TAB>", function()buf_buddy.open_ui("float")end, {desc="BufBuddy"})
     vim.keymap.set("n", "<M-1>", function()buf_buddy.select(1)end, {desc="[BufBuddy] Buffer 1"})
     vim.keymap.set("n", "<M-2>", function()buf_buddy.select(2)end, {desc="[BufBuddy] Buffer 2"})
     vim.keymap.set("n", "<M-3>", function()buf_buddy.select(3)end, {desc="[BufBuddy] Buffer 3"})
     vim.keymap.set("n", "<M-4>", function()buf_buddy.select(4)end, {desc="[BufBuddy] Buffer 4"})
-    vim.keymap.set("n", "<M-5>", function()buf_buddy.select(5)end, {desc="[BufBuddy] Buffer 5"})
+    vim.keymap.set("n", "<m-5>", function()buf_buddy.select(5)end, {desc="[bufbuddy] buffer 5"})
   end
 }
